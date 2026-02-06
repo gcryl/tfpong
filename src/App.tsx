dad35f777ab08fbe7612d17d775cdaed255b9846
gcryl <cyril.rocheteau@gmail.com>
2026-02-06 21:04:46 +0100

    react ale pong demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
 import { useState } from 'react'
+import { useEffect, useId, useRef } from "react";
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import createALEModule from '../public/ale';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [playing, setPlaying] = useState(false);
+  const aleCanvasId = useId();
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+
+  async function playAGame () {
+    const ALE = await createALEModule();
+    const ale = new ALE.ALEInterface();
+
+    ale.loadROM('roms/pong.bin');
+    ale.resetGame();
+
+    let animationId : number;
+
+    function loop() {
+      if (!ale.gameOver()) {
+        const actions = ale.getMinimalActionSet();
+        const action = actions[Math.floor(Math.random() * actions.length)];
+        ale.renderToCanvas (aleCanvasId)
+        ale.act(action); 
+        animationId = requestAnimationFrame(loop);
+      } else {
+        cancelAnimationFrame(animationId);
+        setPlaying(false)
+      }
+    }
+    loop()
+  } 
+ useEffect(() => {
+   if (playing) 
+      playAGame();
+  }, [playing]);
+  
 
   return (
     <>
@@ -18,11 +53,21 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+
+         <button onClick={() => setPlaying(true)} disabled={playing}>
+        Click to play
         </button>
+
+        <canvas id={aleCanvasId} ref={canvasRef} 
+        width="200"
+        height="100"
+        style={{ border: "1px solid #d3d3d3" }}
+      ></canvas>
+
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Click <a href="/example.html"> here </a> for ale js demo from 
+          <a href="https://github.com/Farama-Foundation/Arcade-Learning-Environment"> Arcade-Learning-Environment.
+         </a>
         </p>
       </div>
       <p className="read-the-docs">
