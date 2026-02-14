7108299e6e5321ac0d0536c7968cd385024bc638
gcryl <cyril.rocheteau@gmail.com>
2026-02-14 11:31:34 +0100

    contextroot

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import commonjs from 'vite-plugin-commonjs';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  commonjs()],
-  base: '/dist/', // IMPORTANT
+  base: '/tfpong/', // IMPORTANT
   worker: {
     plugins: () => [commonjs()],
   }
